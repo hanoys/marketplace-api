8a368fbdefa5b688285d2b7ef150eddd8b3d2c19
hanoys <hanoys@yandex.ru>
2024-03-28 13:05:05 +0300

    set min password and login length

--- internal/handler/dto/user.go
+++ internal/handler/dto/user.go
@@ -1,11 +1,11 @@
 package dto
 
 type SignUpDTO struct {
-	Login    string `json:"login" binding:"required,max=64"`
-	Password string `json:"password" binding:"required,max=64"`
+	Login    string `json:"login" binding:"required,max=64,min=4"`
+	Password string `json:"password" binding:"required,max=64,min=8"`
 }
 
 type LogInDTO struct {
-	Login    string `json:"login" binding:"required,max=64"`
-	Password string `json:"password" binding:"required,max=64"`
+	Login    string `json:"login" binding:"required,max=64,min=4"`
+	Password string `json:"password" binding:"required,max=64,min=8"`
 }
